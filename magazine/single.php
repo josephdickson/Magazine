a4cc782243ce81509a8f8d07602d3a3d84fbbde2
Joseph Dickson <joseph.e.dickson@gmmail.com>
2015-05-21 12:22:18 -0700

    created tag and category templates for video

--- magazine/single.php
+++ magazine/single.php
@@ -2,7 +2,7 @@
 
 $post = $wp_query->post;
 			if ( has_tag( 'video' )) {
-				get_header('video');
+				get_header('single');
 				get_template_part('single-tag-video'); } 
 
 
---
+++ magazine/tag-video.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package magazine
+ */
+get_header('single'); ?>
+<!-- tag-video.php -->
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="row article-begin">
+				<?php while ( have_posts() ) : the_post(); ?>
+
+				<div class="columns">
+						<?php get_template_part('acf/acf', 'video'); ?>
+
+						<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+						<?php get_template_part( 'acf/acf' , 'slideshow' ); ?>
+
+						<?php get_template_part( 'acf/acf', 'flexible-fields' ); ?>
+					
+						<?php get_template_part( 'acf/acf' , 'gallery' ); ?>
+
+						<?php // the_post_navigation(); ?>
+
+						<?php get_template_part('template-parts/social', 'sharing-buttons'); ?>
+
+						<?php
+							// If comments are open or we have at least one comment, load up the comment template
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+						?>
+
+					<?php endwhile; // end of the loop. ?>
+
+					<?php get_template_part( 'template-parts/loop', 'tiles' ); ?>
+				</div>
+			</div><!-- .row -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<div class="row">
+	<?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
+<?php get_template_part('template-parts/social', 'sharing-js'); ?>
